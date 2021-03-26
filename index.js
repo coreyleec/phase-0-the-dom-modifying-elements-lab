295fede729ca4882de9dc1a4854541a83fe9ebe4
Corey Colligan <coreyleec91288@gmail.com>
2021-03-26 15:29:00 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove("main");
+
+const newHeader = document.createElement('h1');
+//document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>COREY is the champion</h1>";
